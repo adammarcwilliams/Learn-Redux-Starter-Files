329b76b9ba08cb8b2dd942f033146bf762ad4996
Adam Marc William <marc@rhythm.digital>
2016-11-28 11:35:02 +0000

    Completes Lesson 11

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Provider } from 'react-redux';
 
+import store, { history } from './store'
 import css from './styles/style.styl';
 
-render(<p>Hello</p>, document.getElementById('root'));
+import App from './components/App'
+import PhotoGrid from './components/PhotoGrid'
+import Single from './components/Single'
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path='/' component={App}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path='/view/:postId' component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
+)
+
+render(router, document.getElementById('root'));
