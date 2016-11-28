720f43082f080fa6b561efbddeac783dbc0bf46b
Adam Marc William <marc@rhythm.digital>
2016-11-28 12:20:17 +0000

    Completes Lesson 12

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Single extends React.Component {
+
+    render () {
+        return (
+        <div className='single-photo'>
+            I'm the single-photo
+        </div>
+        );
+    }
+}
+
+export default Single;
